c76d7f512c6489676fd8bf3f0e9d2827c2338c75
Matteo Morandini <matteomorandini@matteos-mbp.home>
2017-03-10 12:57:56 +0000

    created alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
+var cardTwo = "king";
+var cardThree = "queen";
 var cardFour = "king";
+
+
+if (cardTwo === cardFour) {alert ('You found  match!');}
+else if (cardOne === cardThree) {alert ('You found a match!')}
+else if (cardThree === cardFour) {alert ('You found a match!');}
+else {alert ('Sorry, try again!')}
